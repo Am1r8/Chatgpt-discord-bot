c3d83d3e1cafb67c9fab9c6a9445e4048fae10ad
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-02-02 12:02:46 -0500

    process

--- openai.py
+++ openai.py
@@ -11,3 +11,7 @@ def chatgpt_response(prompt):
         temperature=1,
         max_tokens=100,
     )
+    response_dict = response.get('choice')
+    if response_dict and len(response_dict) > 0:
+        prompt_response = response_dict[0].get['text']
+    return prompt_response
