78e00b35f99e0b7344a1ce5c679b4acafb944ee1
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-02-02 12:02:33 -0500

    send info to openapi

--- openai.py
+++ openai.py
@@ -2,3 +2,12 @@ import openai
 import os
 
 openai_api = ""
+
+
+def chatgpt_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=1,
+        max_tokens=100,
+    )
