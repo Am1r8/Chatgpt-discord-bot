86a9b35c3d9d3666e09b8675b67ac3ab826ae0ca
Am1r__8 <68626539+CoachAlpha@users.noreply.github.com>
2023-02-02 12:01:02 -0500

    message processing

--- discord.py
+++ discord.py
@@ -7,3 +7,9 @@ discord_token = ""
 class Myclient(discord.Client):
     async def on_ready(self):
         print("Bot is ready")
+
+    async def on_message(self, message):
+        print(message.content)
+        if message.author == self.user:
+            return
+        command, user_message = None, None
